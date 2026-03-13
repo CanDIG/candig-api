e3cd1be0ad3190f51f6b28fb183d9af209ff3f80
fnguyen <OrdinaryNeumann@gmail.com>
2026-03-13 15:31:04 -0400

    Refactor + add g_variants endpoint

--- src/api/beacon_operations.py
+++ src/api/beacon_operations.py
@@ -1,7 +1,7 @@
 import connexion
 
 #from ..beacon.request.handlers import filtering_terms_handler
-from ..beacon.omop import datasets, filtering_terms, individuals
+from ..beacon.omop import datasets, filtering_terms, individuals, variants
 #from ..beacon.omop.schemas import DefaultSchemas
 from ..beacon.response import framework
 from ..beacon.request import RequestParams
@@ -116,4 +116,9 @@ async def post_person(body: dict):
     else:
         retval = build_beacon_boolean_response(records, count, params, lambda x, y: x, schema, discovery_data)
 
+    return retval, 200
+
+async def variants_search(body: dict):
+    params = RequestParams(**body).from_request(body)
+    retval = await variants.get_variants(params)
     return retval, 200
